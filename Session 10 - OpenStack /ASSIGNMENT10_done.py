5532f7ad0fad4df7afd154626623d02980a5afde
semplicementeria <mariapiabuonomo03@gmail.com>
2026-01-26 18:12:12 +0100

    Refactor comments and improve print statements

--- Session 10 - OpenStack /ASSIGNMENT10_done.py
+++ Session 10 - OpenStack /ASSIGNMENT10_done.py
@@ -1,21 +1,21 @@
 import requests
 import json
 
-#DATA CONFIGURATION
-#(Requested endpoint from the assignment)
+#Configuration Part:
+#Here I specify the requested endpoint
 BASE_URL = "http://10.15.2.1"
 AUTH_URL = f"{BASE_URL}:5000/v3/auth/tokens"
 
-#Here I have to replace the information taken from the dashboard
+#The following are the data takn from the dashboard
 USERNAME = "dist-sys-user1"
 PASSWORD = "openstack4DistSys"
 PROJECT_ID = "f6af0003bfee4e32ae3ac2107e3a8eaa"
 
-#Resources' ID (find it in Compute -> Images and Compute -> Flavors)
+#Resource identifications (taken from Compute>Instances, I clicked on mine and see all the identifications
 IMAGE_ID = "ee11a82a-1426-44b9-b11b-8b77e0bef276" 
-FLAVOR_ID = "2" # Solitamente 1 per m1.small
+FLAVOR_ID = "2" 
 
-#AUTHENTICATION (through Keyston)
+#Authentication Part:
 auth_data = {
     "auth": {
         "identity": {
@@ -34,27 +34,27 @@ auth_data = {
     }
 }
 
-print("Authentication token request...")
+print("Authentication token requested...")
 response = requests.post(AUTH_URL, json=auth_data)
 
 if response.status_code != 201:
-    print(f"Error: {response.text}")
+    print(f"Authentication error: {response.text}")
     exit()
 
 token = response.headers['X-Subject-Token']
 headers = {'X-Auth-Token': token, 'Content-Type': 'application/json'}
-print("Token obtained successsfully.")
+print("Success in the authentication")
 
-#NETWORK CREATION (through Neutron)
-net_payload = {"network": {"name": "rete_mariapia_auto"}}
+#Network creation Part:
+net_payload = {"network": {"name": "mariapiaNet"}}
 res_net = requests.post(f"{BASE_URL}:9696/v2.0/networks", headers=headers, json=net_payload).json()
 net_id = res_net['network']['id']
-print(f"Network creata con ID: {net_id}")
+print(f"Network created with the ID: {net_id}")
 
-#SUBNET CREATION (necessary part for the boot)
+#Subnet creation:
 subnet_payload = {
     "subnet": {
-        "name": "subnet_mariapia_auto",
+        "name": "mariapiaSubnet",
         "network_id": net_id,
         "ip_version": 4,
         "cidr": "192.168.100.0/24",
@@ -63,29 +63,28 @@ subnet_payload = {
 }
 res_sub = requests.post(f"{BASE_URL}:9696/v2.0/subnets", headers=headers, json=subnet_payload)
 if res_sub.status_code == 201:
-    print("Subnet created correctly.")
+    print("Subnet has been created")
 else:
     print(f"Subnet error: {res_sub.text}")
     exit()
 
-#VM CREATION (through Nova)
-#Requirement to fullfill from the assignment; the admin has to have my name
+#VM creation
 server_payload = {
     "server": {
-        "name": "vm_mariapia_auto",
+        "name": "mariapiaVM",
         "imageRef": IMAGE_ID,
         "flavorRef": FLAVOR_ID,
-        "adminPass": "Maria_Pia", 
+        "adminPass": "Mariapia", 
         "networks": [{"uuid": net_id}],
-        "security_groups": [{"name": "default"}]
-    }
+        "security_groups": [{"name": "default"}] 
+   }
 }
 
-print("Instance creating...")
+print("Creating the instance...")
 res_server = requests.post(f"{BASE_URL}:8774/v2.1/servers", headers=headers, json=server_payload)
 
 if res_server.status_code == 202:
-    print("The instance has been accepted by the system.")
+    print("Instance has been created!")
     print(json.dumps(res_server.json(), indent=2))
 else:
-    print(f"Error in creating the VM: {res_server.text}")
+    print(f"Error in creating VM: {res_server.text}")
